5834ea95e73c53f8414b5159d8186bdb80bbd692
Steven Goff <steveng9@gmail.com>
2020-11-13 21:09:04 -0600

    WIP day 2 part 2

--- src/2018/02/index.ts
+++ src/2018/02/index.ts
@@ -43,8 +43,37 @@ function functionA(ids: string[]): number {
   return hasTwoCount * hasThreeCount;
 }
 
-function functionB(values: string[]): string {
-  return 'a';
+function functionB(lines: string[]): string {
+  let bestMatch = -1;
+  let matchText = '';
+  const processedLines = [];
+  lines.forEach((line) => {
+    let charactersMatched = 0;
+
+    for (const processedLine of processedLines) {
+      const mismatchIndexes = [];
+      for (let i = 0; i < line.length; i++) {
+        if (line.charAt(i) === processedLine.charAt(i)) {
+          //   console.log(`Index ${i} matches between ${line} and ${processedLine}`);
+          charactersMatched += 1;
+        } else {
+          //   console.log(`Index ${i} DOES NOT match between ${line} and ${processedLine}`);
+          mismatchIndexes.push(i);
+        }
+      }
+
+      if (charactersMatched > bestMatch) {
+        // console.log(`Best match is: ${line} and ${processedLine} with mismatch on ${mismatchIndexes}`);
+        bestMatch = charactersMatched;
+        matchText = `${line.slice(0, mismatchIndexes[0])}${line.slice(mismatchIndexes[0] + 1, line.length)}`;
+        console.log(`Match on ${line} and ${processedLine} has mismatch on ${mismatchIndexes}`);
+      }
+    }
+
+    processedLines.push(line);
+  });
+
+  return matchText;
 }
 
 /* Tests */
@@ -57,8 +86,8 @@ assert.strictEqual(functionB(['abcde', 'fghij', 'klmno', 'pqrst', 'fguij', 'axcy
 
 console.time('Time');
 const resultA = functionA(input);
-// const resultB = functionB(input);
+const resultB = functionB(input);
 console.timeEnd('Time');
 
 console.log('Solution to part 1:', resultA);
-// console.log('Solution to part 2:', resultB);
+console.log('Solution to part 2:', resultB);
