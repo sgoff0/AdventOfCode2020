b098c781e5b0692411a5fe2e65709801fa7432e2
Steven Goff <steveng9@gmail.com>
2020-12-08 00:16:54 -0600

    Refactored solution

--- src/2020/08/index.ts
+++ src/2020/08/index.ts
@@ -4,14 +4,80 @@ import assert from 'assert';
 
 const rawInput = readInput();
 const input = rawInput.split('\n');
-
-interface Parsed {
-  instruction: string;
-  value: number;
+interface Instruction {
+  operation: string;
+  argument: number;
 }
-/* Functions */
 
-function doIt(parsed: Parsed[]) {
+/* Functions */
+function stringToInstructions(values: string[]): Instruction[] {
+  return values.map((i) => {
+    const [operation, argAsString] = i.split(' ');
+    const argument = parseInt(argAsString, 10);
+    return {
+      operation,
+      argument,
+    };
+  });
+}
+
+// Refactored way to solve
+class Computer {
+  instructions: Instruction[];
+
+  duplicates = {};
+  accumulator = 0;
+  index = 0;
+  earlyAbort = false;
+
+  ops = {
+    nop: (arg: number) => {
+      this.index += 1;
+    },
+    jmp: (arg: number) => {
+      this.index += arg;
+    },
+    acc: (arg: number) => {
+      this.accumulator += arg;
+      this.index += 1;
+    },
+  };
+
+  constructor(instructions: Instruction[]) {
+    this.instructions = instructions;
+  }
+
+  run() {
+    while (this.index < this.instructions.length) {
+      if (this.failOnDuplicate() === true) {
+        break;
+      }
+      this.step();
+    }
+
+    return {
+      earlyAbort: this.earlyAbort,
+      accumulator: this.accumulator,
+    };
+  }
+
+  failOnDuplicate() {
+    this.duplicates[this.index] = (this.duplicates[this.index] || 0) + 1;
+    if (this.duplicates[this.index] > 1) {
+      this.earlyAbort = true;
+      return true;
+    }
+    return false;
+  }
+
+  step() {
+    const { operation, argument } = this.instructions[this.index];
+    this.ops[operation](argument);
+  }
+}
+
+// Original way I solved
+function run(parsed: Instruction[]) {
   const dupliateChecks: number[] = parsed.map((i) => 0);
   let accumulator = 0;
   let earlyAbort = false;
@@ -25,14 +91,14 @@ function doIt(parsed: Parsed[]) {
         accumulator,
       };
     }
-    if (parsed[i].instruction === 'nop') {
+    if (parsed[i].operation === 'nop') {
       // do nothing
       i += 1;
-    } else if (parsed[i].instruction == 'acc') {
-      accumulator += parsed[i].value;
+    } else if (parsed[i].operation == 'acc') {
+      accumulator += parsed[i].argument;
       i += 1;
-    } else if (parsed[i].instruction === 'jmp') {
-      i += parsed[i].value;
+    } else if (parsed[i].operation === 'jmp') {
+      i += parsed[i].argument;
     }
     // console.log(parsed[i]);
   }
@@ -43,40 +109,27 @@ function doIt(parsed: Parsed[]) {
   };
 }
 
-function parse(values: string[]) {
-  return values.map((i) => {
-    const [instruction, valueAsString] = i.split(' ');
-    const value = parseInt(valueAsString, 10);
-    return {
-      instruction,
-      value,
-    };
-  });
-}
-
 function part1(values: string[]) {
-  return doIt(parse(values));
+  //   return run(stringToInstructions(values));
+  return new Computer(stringToInstructions(values)).run();
 }
 
 function part2(values: string[]) {
-  const parsed = parse(values);
+  const parsed = stringToInstructions(values);
 
   for (let i = 0; i < values.length; i++) {
     let skip = false;
-    const cloned = _.cloneDeep(parsed);
-    if (cloned[i].instruction === 'nop') {
-      cloned[i].instruction = 'jmp';
-    } else if (cloned[i].instruction === 'jmp') {
-      cloned[i].instruction = 'nop';
+    const cloned: Instruction[] = _.cloneDeep(parsed);
+    if (cloned[i].operation === 'nop') {
+      cloned[i].operation = 'jmp';
+    } else if (cloned[i].operation === 'jmp') {
+      cloned[i].operation = 'nop';
     } else {
-      //skip
       skip = true;
     }
 
     if (!skip) {
-      //   console.log('\nIndex ' + i);
-      const result = doIt(cloned);
-
+      const result = new Computer(cloned).run();
       if (result.earlyAbort == false) {
         return result;
       }
@@ -84,6 +137,11 @@ function part2(values: string[]) {
   }
   return 0;
 }
+
+/* Tests */
+
+assert.deepStrictEqual(part1(input), { accumulator: 1331, earlyAbort: true });
+assert.deepStrictEqual(part2(input), { accumulator: 1121, earlyAbort: false });
 
 console.time('Time');
 const resultPart1 = part1(input);
