dd94279bdbf46effef72884c35d89aa99bcdd7fd
Steven Goff <steveng9@gmail.com>
2020-12-09 00:15:33 -0600

    Day 9 complete

--- src/2020/09/index.ts
+++ src/2020/09/index.ts
@@ -5,69 +5,43 @@ const rawInput = readInput();
 const input = rawInput.split('\n').map(Number);
 
 /* Functions */
-
 const hasTwoDistinctElementSummation = (expected: number, list: number[]) =>
-  list.some((a) => list.some((b) => a + b == expected && a != b));
+  list.some((a, i) => list.slice(i + 1).some((b) => a + b == expected && a != b));
 
-const findEncodingError = (preamble: number, list: number[]): number =>
-  list.slice(preamble).find((value, i) => !hasTwoDistinctElementSummation(value, list.slice(i, i + preamble)));
-
-function part1(values: number[]) {
-  return findEncodingError(25, values);
-}
+const findEncodingError = (preamble: number, xmas: number[]): number =>
+  xmas.slice(preamble).find((value, i) => !hasTwoDistinctElementSummation(value, xmas.slice(i, i + preamble)));
 
 function findContiguousSummation(expected: number, list: number[]) {
-  // Option 1: ~8.9ms
-  //   let retVal;
-  //   list.forEach((value, i) => {
-  //     let sum = value;
-  //     return list.slice(i + 1).some((value2, j) => {
-  //       sum += value2;
-  //       if (sum == expected) {
-  //         retVal = { i, j: j + i };
-  //         return true;
-  //       }
-  //     });
-  //   });
-  //   return retVal;
-
-  // Option 2: 31.6ms
-  //   for (const [i, value] of list.entries()) {
-  //     let sum = value;
-  //     for (const [j, value2] of list.slice(i + 1).entries()) {
-  //       sum += value2;
-  //       if (sum == expected) {
-  //         return { i, j: j + i };
-  //       }
-  //     }
-  //   }
-
-  // Option 3: 6.4-7.5ms
   for (let i = 0; i < list.length; i++) {
     let sum = list[i];
     for (let j = i + 1; j < list.length; j++) {
       sum += list[j];
       if (sum == expected) {
-        return { i, j };
+        return { start: i, end: j, list: list.slice(i, j + 1) };
       }
     }
   }
 }
-function part2(values: number[]): number {
-  const requiredSummation = findEncodingError(25, values);
-  const retVal = findContiguousSummation(requiredSummation, values);
-  const sumVals = values.slice(retVal.i, retVal.j + 1);
-  const min = Math.min(...sumVals);
-  const max = Math.max(...sumVals);
-  return min + max;
+
+function part1(xmas: number[]) {
+  return findEncodingError(25, xmas);
+}
+
+function part2(xmas: number[]): number {
+  const requiredSummation = findEncodingError(25, xmas);
+  const { list } = findContiguousSummation(requiredSummation, xmas);
+  return Math.min(...list) + Math.max(...list);
 }
 
 /* Tests */
-
 assert.strictEqual(hasTwoDistinctElementSummation(40, [35, 20, 15, 25, 47]), true);
 assert.strictEqual(hasTwoDistinctElementSummation(40, [20, 20]), false);
 assert.strictEqual(hasTwoDistinctElementSummation(40, [20, 20, 21, 19]), true);
-// assert.strictEqual(part1(input).index, 508);
+assert.strictEqual(findEncodingError(3, [20, 20, 21, 41, 41, 99]), 99);
+assert.strictEqual(findEncodingError(2, [20, 20, 21, 41, 41, 99]), 21);
+assert.strictEqual(findEncodingError(6, [20, 20, 21, 41, 41, 99, 1]), 1);
+assert.deepStrictEqual(findContiguousSummation(3, [1, 5, 1, 1, 1, 2, 3]), { start: 2, end: 4, list: [1, 1, 1] });
+
 assert.strictEqual(part1(input), 22406676);
 assert.strictEqual(part2(input), 2942387);
 
