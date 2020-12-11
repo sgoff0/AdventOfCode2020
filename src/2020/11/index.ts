9ad2a6687508750f291c830a850693792803d7b6
Steven Goff <steveng9@gmail.com>
2020-12-11 01:29:56 -0600

    Grr can't seem to get day 2

--- src/2020/11/index.ts
+++ src/2020/11/index.ts
@@ -33,7 +33,6 @@ const directions = [
 
 const adjacentCount = (layout: Status[][], x: number, y: number) => {
   return directions.reduce((acc, [yMod, xMod]) => acc + isOccupied(layout, x + xMod, y + yMod), 0);
-  //   return directions.reduce((acc, [colMod, rowMod]) => acc + isOccupied(layout, row + rowMod, col + colMod), 0);
 };
 
 function getNewSeatStatus(layout: Status[][], x: number, y: number) {
@@ -51,18 +50,6 @@ function getNewSeatStatus(layout: Status[][], x: number, y: number) {
 function parse(values: string[]) {
   return values.map((row) => row.split('').map((i) => i as Status));
 }
-
-// function parseAsFull(values: string[]) {
-//   return values.map((row) =>
-//     row.split('').map((i) => {
-//       const status = i as Status;
-//       if (status === Status.EMPTY) {
-//         return Status.OCCUPIED;
-//       }
-//       return i as Status;
-//     }),
-//   );
-// }
 
 function changeSeats(oldLayout: Status[][], didLayoutChange: boolean): Status[][] {
   if (didLayoutChange === false) {
@@ -86,76 +73,106 @@ function changeSeats(oldLayout: Status[][], didLayoutChange: boolean): Status[][
 }
 
 function part1(values: string[]): number {
-  const initialLayout = parse(values);
+  return changeSeats(parse(values), true).reduce((total, row) => {
+    return total + row.reduce((rowTotal, val) => rowTotal + (val === Status.OCCUPIED ? 1 : 0), 0);
+  }, 0);
+}
 
-  //   const newData = _.cloneDeep(initialLayout);
+function longRangeCount(
+  layout: Status[][],
+  startingX: number,
+  startingY: number,
+  directionX: number,
+  directionY: number,
+  depth = 1,
+): number {
+  const x = startingX + directionX * depth;
+  const y = startingY + directionY * depth;
+  if (x < 0 || y < 0 || x >= layout[0].length || y >= layout.length) {
+    return 0;
+  }
+  const currentValue = layout[y][x];
+  //   console.log(
+  //     `D: ${depth}, Start: (${startingX}, ${startingY}) Dir: (${directionX}, ${directionY}) Pos: (${x}, ${y}), Val: ${currentValue}`,
+  //   );
+  //   console.log(`Checking ${x}, ${y} and got ${currentValue}`);
+  if (currentValue === Status.FLOOR) {
+    // console.log('Found floor, try again deeper');
+    return longRangeCount(layout, startingX, startingY, directionX, directionY, depth + 1);
+  }
+  const retVal = layout[y][x] === Status.OCCUPIED ? 1 : 0;
+  //   console.log(`RV: [${x}, ${y}]`, retVal);
+  return retVal;
+}
 
-  //   const count = 0;
-  //   for (let y = 0; y < initialLayout.length; y++) {
-  //     for (let x = 0; x < initialLayout[0].length; x++) {
-  //       const currentValue = initialLayout[y][x];
-  //       const neighbors = adjacentCount(initialLayout, x, y);
-  //       newData[y][x] = getNewSeatStatus(initialLayout, x, y);
-  //     }
-  //   }
+const longRangeAdjacentCount = (layout: Status[][], x: number, y: number) => {
+  return directions.reduce((acc, [dirX, dirY]) => acc + longRangeCount(layout, x, y, dirX, dirY), 0);
+};
 
-  const layout = changeSeats(initialLayout, true);
-  //   //   const layout = changeSeats(initialLayout, true);
-  //   let isChanged = true;
+function getNewLongRangeSeatStatus(layout: Status[][], x: number, y: number) {
+  const currentSeat = layout[y][x];
+  const neighbors = longRangeAdjacentCount(layout, x, y);
+  if (currentSeat === Status.EMPTY && neighbors === 0) {
+    return Status.OCCUPIED;
+  } else if (currentSeat === Status.OCCUPIED && neighbors >= 5) {
+    return Status.EMPTY;
+  } else {
+    return currentSeat;
+  }
+}
 
-  //   const layout = _.cloneDeep(initialLayout);
-  //   while (isChanged) {
-  //     isChanged = false;
-  //     layout.forEach((rowValue, row) => {
-  //       rowValue.forEach((colValue, col) => {
-  //         const newStatus = getNewSeatStatus(layout, row, col);
-  //         if (newStatus !== colValue) {
-  //           isChanged = true;
-  //           layout[row][col] = newStatus;
-  //           //   rowValue[col] = newStatus;
-  //         }
-  //       });
-  //     });
-  //   }
+function changePart2Seats(oldLayout: Status[][], didLayoutChange: boolean): Status[][] {
+  if (didLayoutChange === false) {
+    return oldLayout;
+  }
 
-  let count = 0;
-  layout.forEach((row) => {
-    row.forEach((col) => {
-      if (col === Status.OCCUPIED) {
-        count += 1;
+  const newList = oldLayout.map((r) => r.map((c) => c));
+
+  let isChanged = false;
+  oldLayout.forEach((rowValue, y) => {
+    rowValue.forEach((colValue, x) => {
+      const newStatus = getNewLongRangeSeatStatus(oldLayout, x, y);
+      if (newStatus !== colValue) {
+        isChanged = true;
+        newList[y][x] = newStatus;
       }
     });
   });
-  return count;
+
+  return changeSeats(newList, isChanged);
 }
 
 function part2(values: string[]): number {
-  return 0;
+  return changePart2Seats(parse(values), true).reduce((total, row) => {
+    return total + row.reduce((rowTotal, val) => rowTotal + (val === Status.OCCUPIED ? 1 : 0), 0);
+  }, 0);
 }
 
-const testEmpty = [
-  [Status.FLOOR, Status.EMPTY, Status.EMPTY, Status.EMPTY],
-  [Status.FLOOR, Status.EMPTY, Status.EMPTY, Status.EMPTY],
-  [Status.FLOOR, Status.EMPTY, Status.EMPTY, Status.EMPTY],
-  [Status.FLOOR, Status.EMPTY, Status.EMPTY, Status.EMPTY],
+const test1 = [
+  [Status.FLOOR, Status.EMPTY, Status.FLOOR, Status.OCCUPIED],
+  [Status.FLOOR, Status.FLOOR, Status.FLOOR, Status.EMPTY],
+  [Status.FLOOR, Status.FLOOR, Status.EMPTY, Status.OCCUPIED],
+  [Status.FLOOR, Status.OCCUPIED, Status.EMPTY, Status.EMPTY],
 ];
-const testFilled = [
-  [Status.OCCUPIED, Status.OCCUPIED, Status.OCCUPIED, Status.OCCUPIED],
-  [Status.OCCUPIED, Status.OCCUPIED, Status.OCCUPIED, Status.OCCUPIED],
-  [Status.OCCUPIED, Status.OCCUPIED, Status.OCCUPIED, Status.OCCUPIED],
-  [Status.OCCUPIED, Status.OCCUPIED, Status.OCCUPIED, Status.OCCUPIED],
-];
+// const testFilled = [
+//   [Status.OCCUPIED, Status.OCCUPIED, Status.OCCUPIED, Status.OCCUPIED],
+//   [Status.OCCUPIED, Status.OCCUPIED, Status.OCCUPIED, Status.OCCUPIED],
+//   [Status.OCCUPIED, Status.OCCUPIED, Status.OCCUPIED, Status.OCCUPIED],
+//   [Status.OCCUPIED, Status.OCCUPIED, Status.OCCUPIED, Status.OCCUPIED],
+// ];
 
-// assert.strictEqual(getNewSeatStatus(testEmpty, 0, 0), Status.FLOOR);
+assert.strictEqual(longRangeAdjacentCount(test1, 1, 0), 3);
 // assert.strictEqual(getNewSeatStatus(testEmpty, 0, 1), Status.OCCUPIED);
 // assert.strictEqual(getNewSeatStatus(testEmpty, 0, 2), Status.OCCUPIED);
 
 // assert.strictEqual(getNewSeatStatus(testFilled, 0, 0), Status.OCCUPIED);
 // assert.strictEqual(getNewSeatStatus(testFilled, 1, 2), Status.EMPTY);
+// assert.strictEqual(part1(input), 2346);
 
-const resultPart1 = part1(input); // NOT 2723, 2693, 2954, 3809
-// const resultPart2 = part2(input);
+console.time('Time');
+// const resultPart1 = part1(input); // NOT 2723, 2693, 2954, 3809
+const resultPart2 = part2(input);
 console.timeEnd('Time');
 
-console.log('Solution to part 1:', resultPart1);
-// console.log('Solution to part 2:', resultPart2);
+// console.log('Solution to part 1:', resultPart1);
+console.log('Solution to part 2:', resultPart2);
