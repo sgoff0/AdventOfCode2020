e8d60a6aa77d06e0ab5e9581ba533ea1ce5707cb
Steven Goff <steveng9@gmail.com>
2020-12-20 02:11:48 -0600

    Day 20 part 1 complete

--- src/2020/20/index.ts
+++ src/2020/20/index.ts
@@ -13,39 +13,18 @@ type KeyedGrid = {
 };
 
 let uglyGlobalResponse: KeyedGrid[];
-// insides dont' matter
 function part1(values: string): number {
   const tiles = values.split('\n\n');
-
-  const grids = tiles.map((tile, i) => {
+  const grids = tiles.map((tile) => {
     const [titleRaw, ...image] = tile.split('\n');
-
     const title = /(\d+)/.exec(titleRaw)[1];
-
     const parsed = image.map((row) => row.split('').map((i) => i));
-
     const grid = new Grid(parsed);
-
     return { title, grid };
-    // grid.rotate();
   });
 
   const size = Math.sqrt(grids.length);
-  //   for (let y = 0; y < size; y++) {
-  //     for (let x = 0; x < size; x++) {
-
-  //     }
-  //   }
-  //   console.log(`I Have ${tiles.length} tiles`);
-
-  const retVal = alignGrid(
-    [],
-    // grids.map((v) => v.grid),
-    grids,
-    size,
-  );
-  console.log('RV: ', retVal);
-  console.log('RV: ', uglyGlobalResponse);
+  alignGrid([], grids, size);
   return (
     +uglyGlobalResponse[0].title *
     +uglyGlobalResponse[size - 1].title *
@@ -54,58 +33,40 @@ function part1(values: string): number {
   );
 }
 
-// function getOrientationsToProcess(grid: Grid<string>, size: number, position: Vector2) {
-//   const toProcess = [];
-//   if (position.x !== 0) {
-//     toProcess.push(grid.alignsCurry(Direction.West));
-//   }
-//   if (position.y !== 0) {
-//     toProcess.push(grid.alignsCurry(Direction.North));
-//     //   checkNorth = false;
-//   }
-//   if (position.y !== size - 1) {
-//     toProcess.push(grid.alignsCurry(Direction.South));
-//     //   checkSouth = false;
-//   }
-//   if (position.x !== size - 1) {
-//     toProcess.push(grid.alignsCurry(Direction.East));
-//     //   checkEast = false;
-//   }
-//   return toProcess;
-// }
+function getAllGridOrientations(grid: Grid<string>) {
+  const grid1 = grid;
+  const grid2 = grid1.rotate();
+  const grid3 = grid2.rotate();
+  const grid4 = grid3.rotate();
+  // DIDN'T SEE FLIP RULES FOR 2 HOURS OMG
+  const grid5 = grid1.flipOnYPlane();
+  const grid6 = grid5.rotate();
+  const grid7 = grid6.rotate();
+  const grid8 = grid7.rotate();
 
-// Only check trailing orientations as I try to move forward, e.g. never check east or south
-// function getOrientationsToProcess(grid: Grid<string>, size: number, position: Vector2) {
-//   const toProcess = [];
-//   if (position.x !== 0) {
-//     toProcess.push(grid.alignsCurry(Direction.West));
-//   }
-//   if (position.y !== 0) {
-//     toProcess.push(grid.alignsCurry(Direction.North));
-//     //   checkNorth = false;
-//   }
-//   //   if (position.y !== size - 1) {
-//   //     toProcess.push(grid.alignsCurry(Direction.South));
-//   //     //   checkSouth = false;
-//   //   }
-//   //   if (position.x !== size - 1) {
-//   //     toProcess.push(grid.alignsCurry(Direction.East));
-//   //     //   checkEast = false;
-//   //   }
-//   return toProcess;
-// }
+  return [grid1, grid2, grid3, grid4, grid5, grid6, grid7, grid8];
+}
 
-// ONLY rotate one grid
-// function canMatch(f: (a: Grid<string>) => boolean, nextGrid: Grid<string>): boolean {
-//   let toCheck = nextGrid;
-//   for (let i = 0; i < 4; i++) {
-//     if (f(toCheck)) {
-//       return true;
-//     }
-//     toCheck = new Grid(toCheck.rotate());
-//   }
-//   return false;
-// }
+const gridAlignsWithPrior = (
+  decisions: KeyedGrid[],
+  size: number,
+  westGridIndex: number,
+  northGridIndex: number,
+  x: number,
+  y: number,
+) => (grid: Grid<string>) => {
+  if (x !== 0) {
+    if (!grid.alignsXWardWith(Direction.West, decisions[westGridIndex].grid)) {
+      return false;
+    }
+  }
+  if (y !== 0) {
+    if (!grid.alignsXWardWith(Direction.North, decisions[northGridIndex].grid)) {
+      return false;
+    }
+  }
+  return true;
+};
 
 /**
  * Store Grid of chosen decisions, check backwards then commit
@@ -118,45 +79,20 @@ function part1(values: string): number {
  * @param depth
  */
 function alignGrid(decisions: KeyedGrid[] = [], gridsRemaining: KeyedGrid[], size: number, depth = 0) {
-  const x = depth % size;
-  const y = Math.floor(depth / size);
   if (gridsRemaining.length == 0) {
     uglyGlobalResponse = decisions;
     return true;
   }
+  const x = depth % size;
+  const y = Math.floor(depth / size);
+  const westGridIndex = depth - 1;
+  const northGridIndex = depth - size;
   // If I pass all checks, move forward
   // check every grid
   return gridsRemaining.some((rawGrid, i) => {
     // Get every orientation of this grid
-    const grid1 = new Grid(rawGrid.grid.values);
-    const grid2 = grid1.rotate();
-    const grid3 = grid2.rotate();
-    const grid4 = grid3.rotate();
-    // DIDN'T SEE FLIP RULES FOR 2 HOURS OMG
-    const grid5 = grid1.flipOnYPlane();
-    const grid6 = grid5.rotate();
-    const grid7 = grid6.rotate();
-    const grid8 = grid7.rotate();
-
-    return [grid1, grid2, grid3, grid4, grid5, grid6, grid7, grid8]
-      .filter((grid, i) => {
-        if (x !== 0) {
-          if (!grid.alignsXWardWith(Direction.West, decisions[depth - 1].grid)) {
-            return false;
-          }
-        }
-        if (y !== 0) {
-          // must verify north
-          // north would be decision depth - size
-          if (depth == 8) {
-            console.log(`Comparing ${rawGrid.title} northward to ${decisions[depth - size].title}`);
-          }
-          if (!grid.alignsXWardWith(Direction.North, decisions[depth - size].grid)) {
-            return false;
-          }
-        }
-        return true;
-      })
+    return getAllGridOrientations(rawGrid.grid)
+      .filter(gridAlignsWithPrior(decisions, size, westGridIndex, northGridIndex, x, y))
       .some((gridOrientation: Grid<string>) => {
         const newDecision = [...decisions, { title: rawGrid.title, grid: gridOrientation }];
         const otherGrids = [...gridsRemaining.slice(0, i), ...gridsRemaining.slice(i + 1)];
@@ -175,8 +111,6 @@ assert.deepStrictEqual(testGrid, new Grid(testGrid.values));
 assert.deepStrictEqual(testGrid, new Grid(testGrid.values).rotate().rotate().rotate().rotate());
 assert.deepStrictEqual(testGrid, new Grid(testGrid.values).flipOnYPlane().flipOnYPlane());
 
-console.log(testGrid);
-
 function part2(values: number[]): number {
   return 0;
 }
@@ -188,11 +122,7 @@ const resultPart1 = part1(rawInput);
 // const resultPart2 = part2(rawInput);
 console.timeEnd('Time');
 
+// console.log('Result: ', part1(demoInput));
 console.log('Solution to part 1:', resultPart1);
 // console.log('Solution to part 2:', resultPart2); // 458 is too high
 assert.strictEqual(part1(rawInput), 15405893262491);
-
-// console.log('Result: ', part1(demoInput));
-// console.log('Result: ', part1(rawInput));
-// // part1(rawInput);
-// console.log('Done');
